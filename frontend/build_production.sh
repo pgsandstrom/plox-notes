ffe2030c8a84d76aa9980e131d427738947f8b59
Per Sandstrom <pg.sandstrom@gmail.com>
2017-04-22 17:54:49 +0200

    Change favico to png

--- frontend/build_production.sh
+++ frontend/build_production.sh
@@ -9,5 +9,5 @@ mkdir ./release
 cp -r ./assets ./release/
 #cp -r ./img ./release/
 cp ./index.html ./release/
-cp ./favicon.ico ./release/
+cp ./favicon.png ./release/
 sed -i 's/bundle_dev.js/bundle_prod.js/' ./release/index.html
