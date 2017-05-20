b91a6543f6721f480ce6860dfcda963f600dddb4
Per Sandstrom <pg.sandstrom@gmail.com>
2017-05-20 14:43:47 +0200

    Make font awesome a dependency

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Router, Route, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import { AppContainer } from 'react-hot-loader';
-import 'normalize.css';
+import 'font-awesome/scss/font-awesome.scss';
 
 import store from './store';
 
