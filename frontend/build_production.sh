3708dd7086db23df5a0cd80f3adee7429a197e31
Per Sandstrom <pg.sandstrom@gmail.com>
2017-05-20 14:09:39 +0200

    Add favicon stuff to release script

--- frontend/build_production.sh
+++ frontend/build_production.sh
@@ -9,5 +9,7 @@ mkdir ./release
 cp -r ./assets ./release/
 #cp -r ./img ./release/
 cp ./index.html ./release/
-cp ./favicon.png ./release/
+cp ./*.png ./release/
+cp ./*.ico ./release/
+cp ./browserconfig.xml ./release/
 sed -i 's/bundle_dev.js/bundle_prod.js/' ./release/index.html
