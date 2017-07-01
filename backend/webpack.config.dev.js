3a232a50166ff6c310b885ec2d278e70a4b7b1c7
Per Sandstrom <per.sandstrom@purplescout.se>
2017-07-01 21:23:47 +0200

    Add dev mode for server side rendering. Intelligent hot swapping for frontend is broken though...

---
+++ backend/webpack.config.dev.js
@@ -0,0 +1,109 @@
+const webpack = require('webpack');
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const serverConfig = {
+  entry: ['./src/index.js'],
+  target: 'node',
+  externals: [nodeExternals()],
+  output: {
+    path: path.resolve(__dirname, 'devbuild'),
+    filename: 'server.bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['airbnb'],
+          },
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: 'null-loader',
+      },
+      {
+        test: /\.css$/,
+        use: 'null-loader',
+      },
+    ],
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.BannerPlugin({
+      banner: 'require("source-map-support").install();',
+      raw: true,
+      entryOnly: false,
+    }),
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};
+
+const clientConfig = {
+  devtool: 'source-map',
+  entry: ['whatwg-fetch', 'babel-polyfill', './client/src/'],
+  output: {
+    publicPath: '/static/',
+    path: path.resolve(__dirname, 'devbuild/public/static'),
+    filename: 'client.bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['airbnb'],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: 'css-loader',
+        }),
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader'],
+        }),
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: 'url-loader?limit=10000&minetype=application/font-woff',
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: 'file-loader',
+      },
+      { test: /\.(jpg|png)$/, use: 'url-loader?limit=25000' },
+    ],
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    // Make react understand we are in dev mode
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('development'),
+      },
+    }),
+    new ExtractTextPlugin('styles.css'),
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};
+
+module.exports = [serverConfig, clientConfig];
