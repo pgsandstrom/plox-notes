c26e21b7a7342ee65ed845d6a308e6656803201c
Per Sandstrom <pg.sandstrom@gmail.com>
2018-03-13 17:47:04 +0100

    fix release script

--- do_complete_deploy.sh
+++ do_complete_deploy.sh
@@ -4,11 +4,12 @@ set -u
 
 cd "$(dirname "$0")"
 
+./prep_repo_for_deploy.sh
+
 cd ./code
 npm install
+npm run build
+sudo cp -r ./dist /apps/bos.se
 cd ..
 
-sudo cp -r ./code/* /apps/bos.se
-cd /apps/bos.se
-npm run build
 pm2 restart bos.se
