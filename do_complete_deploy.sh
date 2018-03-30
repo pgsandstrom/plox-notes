6d2348aec4e2669d934b27774b0a87b6b91c0e0a
Per Sandstrom <pg.sandstrom@gmail.com>
2018-03-30 14:21:54 +0200

    Again with the release script!

--- do_complete_deploy.sh
+++ do_complete_deploy.sh
@@ -10,6 +10,8 @@ cd ./code
 npm install
 npm run build
 sudo cp -r ./dist /apps/bos.se
+sudo cp -r ./node_modules /apps/bos.se
 cd ..
+sudo cp -r ./config/pm2.json /apps/bos.se
 
 pm2 restart bos.se
